daf1e6f78497dbd62a1d6afd4bf0f6db1d02af68
Navneet Dabral <dabralnavneet@gmail.com>
2024-05-19 16:37:27 +0530

    lint fix

--- src/app/components/tech.tsx
+++ src/app/components/tech.tsx
@@ -10,7 +10,7 @@ import {
   IconBrandTypescript,
   IconBrandGit,
   IconBrandVscode,
-    IconBrandNodejs,
+  IconBrandNodejs,
 } from '@tabler/icons-react';
 
 const tech = [
@@ -31,9 +31,12 @@ const tech = [
 export const Tech = () => {
   return (
     <div className="w-full items-center grid grid-cols-2 md:grid-cols-4 gap-4 mt-4 sm:mt-12">
-      {tech.map((Icon) => (
-        <div className="flex justify-center py-10  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <Icon className='opacity-50' size={96} />
+      {tech.map((Icon, index) => (
+        <div
+          key={index}
+          className="flex justify-center py-10  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <Icon className="opacity-50" size={96} />
         </div>
       ))}
     </div>
